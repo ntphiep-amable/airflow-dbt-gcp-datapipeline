aef621e854625dca0b54bdf73e03d6e3a2472948
Nguyen Hiep <hiepghast23@gmail.com>
2023-12-27 15:16:12 +0700

    GCP task

---
+++ dags/retail.py
@@ -0,0 +1,49 @@
+from airflow.decorators import dag, task
+from datetime import datetime, timedelta
+
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
+from astro.constants import FileType
+
+@dag(
+    start_date=datetime(2023, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=["retail"],
+)
+def retail():
+
+    upload_csv_to_gcs = LocalFilesystemToGCSOperator(
+        task_id="upload_csv_to_gcs",
+        src="/usr/local/airflow/include/dataset/online_retail.csv",
+        dst="raw/online_retail.csv",
+        bucket="hugo-page-7725d.appspot.com",
+        gcp_conn_id="gcp",
+        mime_type="text/csv",
+    )
+
+    create_retail_dataset = BigQueryCreateEmptyDatasetOperator(
+        task_id="create_retail_dataset",
+        dataset_id="retail",
+        gcp_conn_id="gcp",
+    )
+
+    gcs_to_raw = aql.load_file(
+        task_id="gcs_to_raw",
+        input_file=File(
+            'gs://hugo-page-7725d.appspot.com/raw/online_retail.csv',
+            conn_id="gcp",
+            filetype=FileType.CSV,
+        ),
+        output_table=Table(
+            name='raw_incoices',
+            conn_id="gcp",
+            metadata=Metadata(schema="retail"),
+        ),
+        use_native_support=False,
+    )
+
+retail()
