f00a7600736ba1e54f67aedf58fdf4cb2eb617b9
Nguyen Hiep <hiepghast23@gmail.com>
2023-12-27 17:56:35 +0700

    final pipeline

--- dags/retail.py
+++ dags/retail.py
@@ -3,10 +3,20 @@ from datetime import datetime, timedelta
 
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+from airflow.models.baseoperator import chain
+
 from astro import sql as aql
 from astro.files import File
 from astro.sql.table import Table, Metadata
 from astro.constants import FileType
+
+from include.dbt.cosmos_config import DBT_PROJECT_CONFIG, DBT_CONFIG
+from cosmos.airflow.task_group import DbtTaskGroup
+from cosmos.constants import LoadMode
+from cosmos.config import ProjectConfig, RenderConfig
+
+
+
 
 @dag(
     start_date=datetime(2023, 1, 1),
@@ -47,13 +57,62 @@ def retail():
     )
 
 
-    @task.external_python(python='usr/local/airflow/soda_venv/bin/python')
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
     def check_load(scan_name='check_load', checks_subpath='sources'):
         from include.soda.check_function import check
 
         return check(scan_name, checks_subpath)
     
     
-    check_load = check_load()
+    
+    transform = DbtTaskGroup(
+            group_id='transform',
+            project_config=DBT_PROJECT_CONFIG,
+            profile_config=DBT_CONFIG,
+            render_config=RenderConfig(
+                load_method=LoadMode.DBT_LS,
+                select=['path:models/transform']
+            )
+        )
+
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_transform(scan_name='check_transform', checks_subpath='transform'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+    
+
+
+    
+
+    report = DbtTaskGroup(
+        group_id='report',
+        project_config=DBT_PROJECT_CONFIG,
+        profile_config=DBT_CONFIG,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS,
+            select=['path:models/report']
+        )
+    )
+
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_report(scan_name='check_report', checks_subpath='report'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+    
+
+
+    # the final pipeline
+    chain(
+        upload_csv_to_gcs,
+        create_retail_dataset,
+        gcs_to_raw,
+        check_load(),
+        transform,
+        check_transform(),
+        report,
+        check_report()
+    )
 
 retail()
