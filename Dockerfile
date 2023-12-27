fe9d769ef92881dbd8eb6a3109509497107bc970
Nguyen Hiep <hiepghast23@gmail.com>
2023-12-27 16:39:00 +0700

    quality checking with Soda

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,8 @@
 FROM quay.io/astronomer/astro-runtime:10.0.0
+
+
+
+# install soda into a virtual environment
+RUN python -m venv soda_venv && source soda_venv/bin/activate && \
+    pip install --no-cache-dir soda-core-bigquery==3.0.45 &&\
+    pip install --no-cache-dir soda-core-scientific==3.0.45 && deactivate
--- dags/retail.py
+++ dags/retail.py
@@ -46,4 +46,14 @@ def retail():
         use_native_support=False,
     )
 
+
+    @task.external_python(python='usr/local/airflow/soda_venv/bin/python')
+    def check_load(scan_name='check_load', checks_subpath='sources'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+    
+    
+    check_load = check_load()
+
 retail()
